6c02a138f6e1c69465222bbca8422fd33e88a0ed
Matheus Henrique <matheusvita007@gmail.com>
2026-05-04 21:09:33 -0300

    feat: criar JWTAuthorizationFilter.java

--- src/main/java/com/matheushenrique/todosimple/configs/SecurityConfig.java
+++ src/main/java/com/matheushenrique/todosimple/configs/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.matheushenrique.todosimple.configs;
 
 import com.matheushenrique.todosimple.Security.JWTAuthenticationFilter;
+import com.matheushenrique.todosimple.Security.JWTAuthorizationFilter;
 import com.matheushenrique.todosimple.Security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -61,6 +62,8 @@ public class SecurityConfig {
                 .authenticationManager(authenticationManager);
 
         http.addFilter(new JWTAuthenticationFilter(this.authenticationManager, this.jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(this.authenticationManager, this.jwtUtil, this.userDetailsService));
+
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
